e40ed7be3a862f7ad1099c743319a2e20ff77976
flyduck-dev <flyduck0dev@gmail.com>
2023-05-01 02:49:23 +0000

    Argument Passing

--- userprog/process.c
+++ userprog/process.c
@@ -39,23 +39,25 @@ process_init (void) {
  * thread id, or TID_ERROR if the thread cannot be created.
  * Notice that THIS SHOULD BE CALLED ONCE. */
 tid_t
-process_create_initd (const char *file_name) { //command line의 전체가 매개변수로 들어옴
+process_create_initd (const char *file_name) {
 	char *fn_copy;
 	tid_t tid;
+	char *save_ptr;
+	char *token;
 
 	/* Make a copy of FILE_NAME.
-	 * Otherwise there's a race between the caller and load(). */
+	 * Otherwise there's a race between the caller and load()*/
+	
 	fn_copy = palloc_get_page (0);
 	if (fn_copy == NULL)
 		return TID_ERROR;
-	strlcpy (fn_copy, file_name, PGSIZE); //file_name 문자열을 fn_copy 문자열에 복사
-	
-	char *save_ptr;
+	strlcpy (fn_copy, file_name, PGSIZE);
+	token = strtok_r (file_name, " ", &save_ptr);
 
-	char *token = strtok_r(file_name, " ", &save_ptr);
 
 	/* Create a new thread to execute FILE_NAME. */
-	tid = thread_create (file_name, PRI_DEFAULT, initd, fn_copy); //file_name 대가리만 넣어서 끝났다
+	tid = thread_create (file_name, PRI_DEFAULT, initd, fn_copy);
+
 	if (tid == TID_ERROR)
 		palloc_free_page (fn_copy);
 	return tid;
@@ -165,35 +167,38 @@ error:
 /* Switch the current execution context to the f_name.
  * Returns -1 on fail. */
 int
-process_exec (void *f_name) { //대가리 only
+process_exec (void *f_name) {
 	char *file_name = f_name;
 	bool success;
+
 
 	/* We cannot use the intr_frame in the thread structure.
 	 * This is because when current thread rescheduled,
 	 * it stores the execution information to the member. */
 	struct intr_frame _if;
+	// 인터럽트 스택 프레임
 	_if.ds = _if.es = _if.ss = SEL_UDSEG;
 	_if.cs = SEL_UCSEG;
 	_if.eflags = FLAG_IF | FLAG_MBS;
 
-	// 파일과 옵션을 분리하는 것이 1차과제 implement the argument passing
-	// 현재 실행중인 스레드의 context를 frame에 해당하는 명령어를 실행시키기 위해 context switch 하는 것이 해당 함수 역할
-	// 즉 이전에 돌고 있던 쓰레드를 context switch 해주기
-
-	// Initialize user stack
-	// set up argument at the user stack
-
 	/* We first kill the current context */
 	process_cleanup ();
+
+	// Command_line_parsing
+	memset(&_if, 0, sizeof _if);
+
+	/*argv에다가 파일을 포함한 아규먼트 들을 담아서 넘겨주기*/
 
 	/* And then load the binary */
 	success = load (file_name, &_if);
 
 	/* If load failed, quit. */
 	palloc_free_page (file_name);
-	if (!success)
+	if (!success){
 		return -1;
+	}
+
+	hex_dump(_if.rsp, _if.rsp, KERN_BASE - _if.rsp, true);
 
 	/* Start switched process. */
 	do_iret (&_if);
@@ -215,6 +220,11 @@ process_wait (tid_t child_tid UNUSED) {
 	/* XXX: Hint) The pintos exit if process_wait (initd), we recommend you
 	 * XXX:       to add infinite loop here before
 	 * XXX:       implementing the process_wait. */
+	while (1)
+	{
+		;
+	}
+	
 	return -1;
 }
 
@@ -340,6 +350,18 @@ load (const char *file_name, struct intr_frame *if_) {
 	bool success = false;
 	int i;
 
+	char *token, *save_ptr;
+	char *argv[128]; // 파싱한 인자들을 저장할 배열
+	int argc = 0;
+ 
+	token = strtok_r(file_name, " ", &save_ptr); 
+
+	while(token!=NULL){
+		token = strtok_r (NULL, " ", &save_ptr);
+		argv[argc++] = token;
+	}
+
+
 	/* Allocate and activate page directory. */
 	t->pml4 = pml4_create ();
 	if (t->pml4 == NULL)
@@ -418,15 +440,25 @@ load (const char *file_name, struct intr_frame *if_) {
 		}
 	}
 
+
 	/* Set up stack. */
-	if (!setup_stack (if_))
+	if (!setup_stack (if_)){
 		goto done;
+	}
+
 
 	/* Start address. */
+	/*ELF 파일의 실행 시작 지점으로 if_->rip 레지스터를 설정하는 것입니다. 
+	이를 통해 커널이 새로운 프로세스를 실행시킬 때, 해당 프로세스의 실행이 ELF 파일의 e_entry 지점부터 시작하게 됩니다.*/
 	if_->rip = ehdr.e_entry;
+	
 
 	/* TODO: Your code goes here.
-	 * TODO: Implement argument passing (see project2/argument_passing.html). */
+	 * TODO: Implement
+	 
+	  (see project2/argument_passing.html). */
+	
+	argument_stack(argv, argc, if_);
 
 	success = true;
 
@@ -434,6 +466,48 @@ done:
 	/* We arrive here whether the load is successful or not. */
 	file_close (file);
 	return success;
+}
+
+/* 인자를 stack에 올린다. */
+void argument_stack(char **argv, int argc, struct intr_frame *if_) { 
+
+	/* insert arguments' address */
+	char *arg_address[128];
+	
+	/* 맨 끝 NULL 값(arg[4]) 제외하고 스택에 저장(arg[0] ~ arg[3]) */
+	for (int i = argc-2; i>=0; i--) { 
+		int argv_len = strlen(argv[i]);
+		if_->rsp = if_->rsp - (argv_len + 1);
+		memcpy(if_->rsp, argv[i], argv_len+1);
+		arg_address[i] = if_->rsp; // arg_address 배열에 현재 문자열 시작 주소 위치를 저장한다.
+	}
+
+	/* word-align: 8의 배수 맞추기 위해 padding 삽입*/
+	while (if_->rsp % 8 != 0) 
+	{
+		if_->rsp--; // 주소값을 1 내리고
+		*(uint8_t *) if_->rsp = 0; //데이터에 0 삽입 => 8바이트 저장
+	}
+
+	/* 이제는 주소값 자체를 삽입! 이때 센티넬 포함해서 넣기*/
+	
+	for (int i = argc; i >=0; i--) 
+	{ // 여기서는 NULL 값 포인터도 같이 넣는다.
+		if_->rsp = if_->rsp - 8; // 8바이트만큼 내리고
+		if (i == argc) { // 가장 위에는 NULL이 아닌 0을 넣어야지
+			memset(if_->rsp, 0, sizeof(char **));
+		} else { // 나머지에는 arg_address 안에 들어있는 값 가져오기
+			memcpy(if_->rsp, &arg_address[i], sizeof(char **)); // char 포인터 크기: 8바이트
+		}	
+	}
+	
+
+	/* fake return address */
+	if_->rsp = if_->rsp - 8; // void 포인터도 8바이트 크기
+	memset(if_->rsp, 0, sizeof(void *));
+
+	if_->R.rdi  = argc;
+	if_->R.rsi = if_->rsp + 8; // fake_address 바로 위: arg_address 맨 앞 가리키는 주소값!
 }
 
 
